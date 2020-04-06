882f88ce0bb3d53428c611602c4d26b676fcacbc
Boisy Pitre <sf@pitre.org>
2020-04-06 08:32:39 +0200

    os9modbust: Add support for OS-9/68K modules

--- os9/os9modbust.c
+++ os9/os9modbust.c
@@ -93,13 +93,121 @@ int os9modbust(int argc, char **argv)
 
 
 
+int modbust_osk(os9_path_id path)
+{
+	error_code ec;
+	u_int size;
+	coco_path_id path2;
+	char name[256];
+	int nameoffset;
+	char buffer[256];
+	u_char *module;
+	coco_file_stat fstat;
+
+	/* We have an OS-9 module - get 2 bytes then 4 byte size */
+	size = 6;
+
+	ec = _os9_read(path, buffer, &size);
+	size = int4((u_char *)buffer + 2);
+	module = (u_char *)malloc(size);
+
+	if (module == NULL)
+	{
+		printf("Memory allocation error\n");
+		return(1);
+	}
+
+	module[0] = OSK_ID0;
+	module[1] = OSK_ID1;
+	module[2] = buffer[0];
+	module[3] = buffer[1];
+	module[4] = buffer[2];
+	module[5] = buffer[3];
+	module[6] = buffer[4];
+	module[7] = buffer[5];
+	size -= 8;
+	ec = _os9_read(path, &module[8], &size);
+	nameoffset = int4(&module[12]);
+	memcpy(name, &module[nameoffset], OS9Strlen(&module[nameoffset]));
+	OS9StringToCString((u_char *)name);
+	printf("Busting module %s...\n", name);
+
+	fstat.perms = FAP_READ | FAP_WRITE;
+	ec = _coco_create(&path2, name, FAM_WRITE, &fstat);
+
+	if (ec != 0)
+	{
+		printf("Error creating file %s\n", name);
+		return(1);
+	}
+
+	size += 8;
+	_coco_write(path2, module, &size);
+	_coco_close(path2);
+
+	free(module);
+
+	return 0;
+}
+
+int modbust_os9(os9_path_id path)
+{
+	error_code ec;
+	u_int size;
+	coco_path_id path2;
+	char name[256];
+	int nameoffset;
+	char buffer[256];
+	u_char *module;
+	coco_file_stat fstat;
+
+	/* We have an OS-9 module - get size */
+	size = 2;
+
+	ec = _os9_read(path, buffer, &size);
+	size = int2((u_char *)buffer);
+	module = (u_char *)malloc(size);
+
+	if (module == NULL)
+	{
+		printf("Memory allocation error\n");
+		return(1);
+	}
+
+	module[0] = 0x87;
+	module[1] = 0xCD;
+	module[2] = buffer[0];
+	module[3] = buffer[1];
+	size -= 4;
+	ec = _os9_read(path, &module[4], &size);
+	nameoffset = int2(&module[4]);
+	memcpy(name, &module[nameoffset], OS9Strlen(&module[nameoffset]));
+	OS9StringToCString((u_char *)name);
+	printf("Busting module %s...\n", name);
+
+	fstat.perms = FAP_READ | FAP_WRITE;
+	ec = _coco_create(&path2, name, FAM_WRITE, &fstat);
+
+	if (ec != 0)
+	{
+		printf("Error creating file %s\n", name);
+		return(1);
+	}
+
+	size += 4;
+	_coco_write(path2, module, &size);
+	_coco_close(path2);
+
+	free(module);
+
+	return 0;
+}
+
 static int do_modbust(char **argv, char *filename)
 {
 	error_code	ec = 0;
 	char buffer[256];
 	os9_path_id path;
-	u_char *module;
-
 
 	ec = _os9_open(&path, filename, FAM_READ);
 
@@ -110,7 +218,7 @@ static int do_modbust(char **argv, char *filename)
     
 	while (_os9_gs_eof(path) == 0)
 	{
-		u_int size = 1;
+		u_int size = 2;
 
 		ec = _os9_read(path, buffer, &size);
 		if (ec != 0)
@@ -120,58 +228,13 @@ static int do_modbust(char **argv, char *filename)
 			return ec;
 		}
      
-		if (buffer[0] == '\x87')
+		if (buffer[0] == (char)OS9_ID0 && buffer[1] == (char)OS9_ID1)
 		{
-			coco_file_stat fstat;
-			size = 1;
-			
-			ec = _os9_read(path, buffer, &size);
-
-			if (buffer[0] == '\xCD')
-			{
-				coco_path_id path2;
-				char name[256];
-				int nameoffset;
-
-				/* We have an OS-9 module - get size */
-				size = 2;
-
-				ec = _os9_read(path, buffer, &size);
-				size = int2((u_char *)buffer);
-				module = (u_char *)malloc(size);
-
-				if (module == NULL)
-				{
-					printf("Memory allocation error\n");
-					return(1);
-				}
-
-				module[0] = 0x87;
-				module[1] = 0xCD;
-				module[2] = buffer[0];
-				module[3] = buffer[1];
-				size -= 4;
-				ec = _os9_read(path, &module[4], &size);
-				nameoffset = int2(&module[4]);
-				memcpy(name, &module[nameoffset], OS9Strlen(&module[nameoffset]));
-				OS9StringToCString((u_char *)name);
-				printf("Busting module %s...\n", name);
-				
-				fstat.perms = FAP_READ | FAP_WRITE;
-				ec = _coco_create(&path2, name, FAM_WRITE, &fstat);
-
-				if (ec != 0)
-				{
-					printf("Error creating file %s\n", name);
-					return(1);
-				}
-
-				size += 4;
-				_coco_write(path2, module, &size);
-				_coco_close(path2);
-
-				free(module);
-			}
+			modbust_os9(path);
+		}
+		else if (buffer[0] == (char)OSK_ID0 && buffer[1] == (char)OSK_ID1)
+		{
+			modbust_osk(path);
 		}
 	}
 
